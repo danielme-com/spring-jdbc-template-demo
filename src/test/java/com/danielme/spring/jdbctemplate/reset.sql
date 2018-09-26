5eb5fd3e6bc1c4ac25772ae8bc4b432b52a335fd
Daniel Medina <danielme_com@yahoo.com>
2018-09-26 19:47:08 +0200

    add id to db script

--- src/test/java/com/danielme/spring/jdbctemplate/reset.sql
+++ src/test/java/com/danielme/spring/jdbctemplate/reset.sql
@@ -1,6 +1,6 @@
 BEGIN;
 TRUNCATE country;
-INSERT INTO `country` (`name`, `population`) VALUES ('Mexico',  130497248);
-INSERT INTO `country` (`name`, `population`) VALUES ('Spain', 49067981);
-INSERT INTO `country` (`name`, `population`) VALUES ('Colombia', 46070146);
+INSERT INTO `country` (`id`,`name`, `population`) VALUES (1, 'Mexico',  130497248);
+INSERT INTO `country` (`id`, `name`, `population`) VALUES (2, 'Spain', 49067981);
+INSERT INTO `country` (`id`,`name`, `population`) VALUES (3, 'Colombia', 46070146);
 COMMIT;

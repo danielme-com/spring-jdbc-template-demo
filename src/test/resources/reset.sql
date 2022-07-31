1b7f3ceac4f53bcf9c389ed2d0b73f40ca62a754
dani <danielme_com@yahoo.com>
2022-07-31 20:03:28 +0200

    adaptar script a dockerfile

--- src/test/resources/reset.sql
+++ src/test/resources/reset.sql
@@ -1,6 +1,6 @@
 BEGIN;
-TRUNCATE country;
-INSERT INTO `country` (`id`,`name`, `population`) VALUES (1, 'Mexico',  130497248);
-INSERT INTO `country` (`id`, `name`, `population`) VALUES (2, 'Spain', 49067981);
-INSERT INTO `country` (`id`,`name`, `population`) VALUES (3, 'Colombia', 46070146);
+TRUNCATE countries;
+INSERT INTO `countries` (`id`,`name`, `population`) VALUES (1, 'Mexico',  130497248);
+INSERT INTO `countries` (`id`, `name`, `population`) VALUES (2, 'Spain', 49067981);
+INSERT INTO `countries` (`id`,`name`, `population`) VALUES (3, 'Colombia', 46070146);
 COMMIT;

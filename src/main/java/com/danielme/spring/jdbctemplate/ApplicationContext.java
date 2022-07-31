b41654ffde926d4e50564258e6027fd901338260
dani <danielme_com@yahoo.com>
2022-07-31 20:03:07 +0200

    cambiar a Hikari

--- src/main/java/com/danielme/spring/jdbctemplate/ApplicationContext.java
+++ src/main/java/com/danielme/spring/jdbctemplate/ApplicationContext.java
@@ -1,9 +1,7 @@
 package com.danielme.spring.jdbctemplate;
 
-import java.beans.PropertyVetoException;
-
-import javax.sql.DataSource;
-
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +10,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
-import com.mchange.v2.c3p0.ComboPooledDataSource;
+import javax.sql.DataSource;
 
 @Configuration
 @PropertySource("classpath:db.properties")
@@ -21,24 +19,12 @@ public class ApplicationContext {
 
     @Bean(destroyMethod = "close")
     DataSource dataSource(Environment env) {
-        ComboPooledDataSource ds = new ComboPooledDataSource();
-        try {
-            ds.setDriverClass(env.getRequiredProperty("jdbc.driverClassName"));
-        } catch (IllegalStateException | PropertyVetoException ex) {
-            throw new RuntimeException(
-                    "error while setting the driver class name in the datasource", ex);
-        }
-        ds.setJdbcUrl(env.getRequiredProperty("jdbc.url"));
-        ds.setUser(env.getRequiredProperty("jdbc.username"));
-        ds.setPassword(env.getRequiredProperty("jdbc.password"));
-        ds.setAcquireIncrement(env.getRequiredProperty("c3p0.acquire_increment", Integer.class));
-        ds.setMinPoolSize(env.getRequiredProperty("c3p0.min_size", Integer.class));
-        ds.setMaxPoolSize(env.getRequiredProperty("c3p0.max_size", Integer.class));
-        ds.setMaxIdleTime(env.getRequiredProperty("c3p0.max_idle_time", Integer.class));
-        ds.setUnreturnedConnectionTimeout(
-                env.getRequiredProperty("c3p0.unreturned_connection_timeout", Integer.class));
-
-        return ds;
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName(env.getRequiredProperty("jdbc.driverClassName"));
+        config.setJdbcUrl(env.getRequiredProperty("jdbc.url"));
+        config.setUsername(env.getRequiredProperty("jdbc.username"));
+        config.setPassword(env.getRequiredProperty("jdbc.password"));
+        return new HikariDataSource(config);
     }
 
     @Bean

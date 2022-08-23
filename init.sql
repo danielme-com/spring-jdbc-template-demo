dce32e2d43ae2321c6ef7b89cc1c3017a051bdcd
dani <danielme_com@yahoo.com>
2022-08-23 20:52:45 +0200

    mejorar el procedimiento y la función

--- init.sql
+++ init.sql
@@ -3,32 +3,37 @@ CREATE USER 'demo'@'%' IDENTIFIED BY 'demo';
 GRANT ALL PRIVILEGES ON `country-test`.* TO 'demo'@'%';
 FLUSH PRIVILEGES;
 USE `country-test`;
-CREATE TABLE `countries` (
-                             `id` SMALLINT NOT NULL AUTO_INCREMENT,
-                             `name` varchar(255) NOT NULL,
-                             `population` int(11) NOT NULL,
-                             PRIMARY KEY (`id`),
-                             UNIQUE KEY `UK_1` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `countries`
+(
+    `id`         SMALLINT     NOT NULL AUTO_INCREMENT,
+    `name`       varchar(255) NOT NULL,
+    `population` int(11)      NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `UK_1` (`name`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = latin1;
 
 COMMIT;
 
 DELIMITER //
-CREATE DEFINER=`root`@`localhost` PROCEDURE `search`(IN name VARCHAR(50), OUT total INT)
+CREATE
+    DEFINER = `root`@`localhost` PROCEDURE `proc_count_countries_by_name`(IN name VARCHAR(50), OUT total INT)
 BEGIN
-    SELECT COUNT(*) INTO total FROM countries c WHERE c.name LIKE name;
+    SELECT COUNT(*) INTO total FROM countries c WHERE c.name LIKE CONCAT('%', @name, '%');
 END//
 DELIMITER ;
 
 SET GLOBAL log_bin_trust_function_creators = 1;
 
 DELIMITER //
-CREATE FUNCTION `search2`(name VARCHAR(50)) RETURNS INT
-BEGIN DECLARE total INT;
-SELECT COUNT(*) INTO total
-FROM countries c
-WHERE c.name LIKE name;
+CREATE FUNCTION `func_count_countries_by_name`(name VARCHAR(50)) RETURNS INT
+BEGIN
+    DECLARE total INT;
+    SELECT COUNT(*)
+    INTO total
+    FROM countries c
+    WHERE c.name LIKE CONCAT('%', @name , '%');
 
-RETURN total;
+    RETURN total;
 END//
 DELIMITER ;
